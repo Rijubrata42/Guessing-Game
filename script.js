c1d177d338673335ba29e87020f0793419d46b5d
Rijubrata42 <rijubratamondal@gmail.com>
2025-01-06 22:22:11 +0530

    enterPress configured

--- script.js
+++ script.js
@@ -45,3 +45,39 @@ document.querySelector('.again').addEventListener('click',function(){
     document.querySelector('.message').textContent='Start guessing...';
     document.querySelector('body').style.backgroundColor='#222';
 })
+
+
+document.querySelector('.guess').addEventListener('keypress',function(e){
+    if(e.key==='Enter' || 'numpadEnter'){
+        const guess=Number(document.querySelector('.guess').value);
+    if(!guess){
+        document.querySelector('.message').textContent='Please Enter a Number❔'
+    }
+    if(guess==number){
+        document.querySelector('.message').textContent='Correct Guess!'
+        document.querySelector('body').style.backgroundColor='#60b347'
+        document.querySelector('.number').textContent=number;
+        if(score>highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent=score
+        }
+    }
+    if(score>1){
+        if(guess>number){
+        document.querySelector('.message').textContent='Guess is too High!📈'
+        score--;
+        document.querySelector('.score').textContent=score;
+        }
+        else if(guess<number){
+            document.querySelector('.message').textContent='Guess is too Low!📉'
+            score--;
+            document.querySelector('.score').textContent=score;
+        }
+    }
+    else{
+        document.querySelector('.message').textContent='You Lost the Game⚔️'
+        document.querySelector('.score').textContent=0;
+    }
+    }
+    
+})
